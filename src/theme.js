f58d5e40d1c63d6610e5de8514a44ec12707d433
Nick Schonning <nschonni@gmail.com>
2015-08-13 00:10:49 -0400

    Build: Bump grunt-contrib-jshint
    Removed the flagged unused parameters in the theme.js

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,7 @@
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
  */
-( function( $, wb ) {
+( function() {
 "use strict";
 
 /*
@@ -75,4 +75,4 @@ window[ "wb-data-ajax" ] = {
 	}
 };
 
-} )( jQuery, wb );
+} )();
